590b579cda92c63fd4ebed44d0023effd583895f
Harry <68451409+leaving-voider@users.noreply.github.com>
2021-08-23 11:04:23 +0800

    Add files via upload

---
+++ upload_from_ios_2_win/app.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+    Flask-upload-dropzone
+    ===================================
+    Summary: flask file upload with Dropzone.js.
+    Author: Grey Li
+    Repository: https://github.com/helloflask/flask-upload-dropzone
+    License: MIT
+"""
+import os
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.config['UPLOADED_PATH'] = os.path.join(app.root_path, 'upload')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        for f in request.files.getlist('file'):
+            f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port='8080')
